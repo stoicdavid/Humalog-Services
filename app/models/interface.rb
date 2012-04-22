0964b293ba8ee11d152c1308106ef35f70a4023f
David Rodriguez <stoicdavid@gmail.com>
2012-04-21 19:22:11 -0500

    done

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -1,5 +1,5 @@
 class Interface < ActiveRecord::Base
   belongs_to :brand
-  attr_accessible :backgrounds_url, :brand_id, :btn_url, :has_closing, :has_ipp, :has_opening, :has_references, :has_special, :has_studies, :nav_btn_url, :number_of_menus, :over_btn_url, :updated_device, :uses_stack_view
+  attr_accessible :backgrounds_url, :brand_id, :btn_url, :has_closing, :has_ipp, :has_opening, :has_references, :has_special,:has_studies, :nav_btn_url, :number_of_menus, :over_btn_url, :updated_device, :uses_stack_view
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120421213712) do
+ActiveRecord::Schema.define(:version => 20120422001844) do
 
   create_table "brands", :force => true do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20120421213712) do
     t.string   "backgrounds_url"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "url_special"
   end
 
   add_index "interfaces", ["brand_id"], :name => "index_interfaces_on_brand_id"
